9a2fbe87ecf36d40b46e7499501dde051d9bd897
Saxashitter* <nicholas.rickys3@gmail.com>
2024-06-12 18:07:51 -0400

    whoopsie leftover smth

--- test.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs")
-const gm = require("gm")
-
-// creating an image
-gm(200, 400, "#ddff99f3")
-.drawText(10, 50, "from scratch")
-.write("mama.jpg", function (err) {
-  // ...
-});
